aeb79f7690bb5e3914d6b432c8d2a43478d8b5fc
Jared Mathews <boat@sea.local>
2023-09-11 09:12:41 -0600

    feature(StewApp): cobble segment plugin loading

--- source/client/app/StewApp.tsx
+++ source/client/app/StewApp.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "npm/preact/hooks";
 import { BuildStewConfig } from "../../shared/types/StewConfig.ts";
 
 export interface StewAppProps {
@@ -6,5 +7,65 @@ export interface StewAppProps {
 
 export function StewApp(props: StewAppProps) {
   const { stewConfig } = props;
-  return <div>loaded</div>;
+  const [segmentKeyState, setSegmentKeyState] = useState(
+    stewConfig.stewSegments[0].segmentKey
+  );
+  const [segmentModuleState, setSegmentModuleState] = useState<any>({
+    fetchStatus: "loading",
+  });
+  useEffect(() => {
+    fetch(`/public/modules/${segmentKeyState}.js`)
+      .then((getModuleIifeScript) => getModuleIifeScript.text())
+      .then((someModuleIifeScript) => {
+        setSegmentModuleState({
+          fetchStatus: "success",
+          segmentModule: new Function(
+            `${someModuleIifeScript}return segmentModuleResult.default`
+          )(),
+        });
+      });
+  }, [segmentKeyState]);
+  const [segmentDatasetState, setSegmentDatasetState] = useState<any>({
+    fetchStatus: "loading",
+  });
+  useEffect(() => {
+    fetch(`/public/datasets/${segmentKeyState}.json`)
+      .then((getDatasetJson) => getDatasetJson.json())
+      .then((someDatasetJson) => {
+        setSegmentDatasetState({
+          fetchStatus: "success",
+          segmentDataset: someDatasetJson,
+        });
+      });
+  }, [segmentKeyState]);
+  return (
+    <div>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        {stewConfig.stewSegments.map((someSegmentConfig) => (
+          <div
+            style={{
+              padding: 8,
+              color: "purple",
+              cursor: "pointer",
+              fontWeight: 700,
+            }}
+            onClick={() => {
+              setSegmentKeyState(someSegmentConfig.segmentKey);
+            }}
+          >
+            {someSegmentConfig.segmentKey}
+          </div>
+        ))}
+      </div>
+      <div style={{ padding: 8, fontWeight: 300 }}>{segmentKeyState}</div>
+      <div>
+        {segmentDatasetState.fetchStatus === "success" &&
+        segmentModuleState.fetchStatus === "success" ? (
+          <segmentModuleState.segmentModule.SegmentItemDisplay
+            someSegmentItem={segmentDatasetState.segmentDataset[0]}
+          />
+        ) : null}
+      </div>
+    </div>
+  );
 }
