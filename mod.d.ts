0c454a2b232d79643b8802d454dc3055b913ebba
Jared Mathews <boat@sea.local>
2023-09-25 12:30:26 -0600

    feature(mod.d.ts): moar typing export modules

--- mod.d.ts
+++ mod.d.ts
@@ -1,4 +1,5 @@
 export type * from "./shared/types/StewConfig.ts";
 export type * from "./shared/types/SegmentModule.ts";
 export type * from "./shared/types/SegmentDataset.ts";
-export { MultiLinkImageItemDisplayProps } from "./source/client/app/components/MultiLinkImageItemDisplay/MultiLinkImageItemDisplay.tsx";
+export type { MultiLinkImageItemDisplayProps } from "./source/client/app/components/MultiLinkImageItemDisplay/MultiLinkImageItemDisplay.tsx";
+export { MultiLinkImageItemDisplay } from "./source/client/app/components/MultiLinkImageItemDisplay/MultiLinkImageItemDisplay.tsx";
