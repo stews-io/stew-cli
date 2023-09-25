eff2685cb02ae343cb8e10ef98cb2a5640d5c279
Jared Mathews <boat@sea.local>
2023-09-25 12:25:05 -0600

    feature(mod.d.ts): adjust types experiment

--- mod.d.ts
+++ mod.d.ts
@@ -1,1 +1,4 @@
-export type * from "./mod.ts";
+export type * from "./shared/types/StewConfig.ts";
+export type * from "./shared/types/SegmentModule.ts";
+export type * from "./shared/types/SegmentDataset.ts";
+export { MultiLinkImageItemDisplayProps } from "./source/client/app/components/MultiLinkImageItemDisplay/MultiLinkImageItemDisplay.tsx";
