3048d2c015694613402859abad6825e046d5129e
Jared Mathews <boat@sea.local>
2023-09-26 07:50:43 -0600

    feature(mod): export general helper types

--- mod.d.ts
+++ mod.d.ts
@@ -1,5 +1,6 @@
 export type * from "./shared/types/StewConfig.ts";
 export type * from "./shared/types/SegmentModule.ts";
 export type * from "./shared/types/SegmentDataset.ts";
+export type * from "./shared/types/general.ts";
 export type { MultiLinkImageItemDisplayProps } from "./source/client/app/components/MultiLinkImageItemDisplay/MultiLinkImageItemDisplay.tsx";
 export { MultiLinkImageItemDisplay } from "./source/client/app/components/MultiLinkImageItemDisplay/MultiLinkImageItemDisplay.tsx";
--- mod.ts
+++ mod.ts
@@ -1,4 +1,5 @@
 export * from "./shared/types/StewConfig.ts";
 export * from "./shared/types/SegmentModule.ts";
 export * from "./shared/types/SegmentDataset.ts";
+export * from "./shared/types/general.ts";
 export * from "./source/client/app/components/MultiLinkImageItemDisplay/MultiLinkImageItemDisplay.tsx";
